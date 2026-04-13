aa9bf940b7af0d3545290c50c907f2c3a88eb11c
Michał Kaczor <mk.kaczor24@gmail.com>
2026-04-13 13:56:35 +0200

    fix: fix a typo in file import

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
 import heapUnderflowImg from "@/assets/HeapUnderflow.webp";
 import reCropImg from "@/assets/ReCrop.webp";
-import trustGateImg from "@/assets/trustgate.webp";
+import trustGateImg from "@/assets/TrustGate.webp";
 import authAppImg from "@/assets/authapp.webp";
 import wospImg from "@/assets/wosp.webp";
 import miniMeteoImg from "@/assets/minimeteo.webp";
